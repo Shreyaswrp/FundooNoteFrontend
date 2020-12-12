633c8c553746acde190902f765776a4f48ff5334
Shreya Swaroop <shreyaswrp@gmail.com>
2020-12-12 22:53:03 +0530

    refactor: post request method

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,13 +3,20 @@ const axios = require('axios');
 export function post(options) {
     console.log(" data in services--> ", options)
     
-    return new Promise((resolve, reject) => {
-        axios.post('http://localhost:5000/register', options.data)
-            .then((response) => {
-                resolve(response)
-            })
-            .catch((error) => {
-                reject(error)
-            })
+    var config = { headers: {
+        'Content-Type': 'application/json'}
+    }
+
+    axios.post('http://localhost:5000/register',{
+        firstName: options.data.firstName,
+        lastName: options.data.lastName,
+        emailId: options.data.email,
+        password: options.data.password
+    },config)
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.log(error);
     })
 }
